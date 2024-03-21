f4c85cb6734a9f05f33959f92217af2e8019e56b
Carlos Cesar Baratella Junior <molusko.rp@hotmail.com>
2024-03-21 09:39:14 -0300

    asd

--- src/routes/localizacao.route.ts
+++ src/routes/localizacao.route.ts
@@ -1,14 +1,35 @@
 import { FastifyInstance } from 'fastify'
 
 export async function localizacaoRoutes(app: FastifyInstance) {
+  const salas = {} as any
+
   app.get('/', { websocket: true }, async (connection, req) => {
     connection.socket.on('message', (message) => {
-      const data = message.toString()
-      connection.socket.send(data)
-    })
+      const data = JSON.parse(message.toString())
+      console.log({ data })
 
-    connection.socket.on('open', () => {
-      console.log('conectou')
+      if (data.type) {
+        if (data.type === 'JOIN') {
+          const salaId = data.salaId
+          if (!salas[salaId]) {
+            salas[salaId] = new Set()
+          }
+          salas[salaId].add(connection.socket)
+          connection.socket.salaId = salaId
+        }
+      } else {
+        const sala = connection.socket.salaId
+        if (sala && salas[sala]) {
+          salas[sala].forEach((client) => {
+            if (client.readyState === connection.socket.OPEN) {
+              client.send(JSON.stringify(data))
+            }
+          })
+        }
+      }
+
+      // const data = message.toString()
+      connection.socket.send(data)
     })
   })
 }
