e8b80e1b7f8ec3f7510f0a555db633f22879182f
Carlos Cesar Baratella Junior <molusko.rp@hotmail.com>
2024-03-21 11:54:15 -0300

    Mais logs

--- src/routes/localizacao.route.ts
+++ src/routes/localizacao.route.ts
@@ -9,7 +9,7 @@ export async function localizacaoRoutes(app: FastifyInstance) {
       console.log({ data })
 
       if (data.type) {
-        console.log('entrou no if data.type')
+        console.log('inicio if data type')
         if (data.type === 'JOIN') {
           const salaId = data.salaId
           if (!salas[salaId]) {
@@ -18,12 +18,14 @@ export async function localizacaoRoutes(app: FastifyInstance) {
           salas[salaId].add(connection.socket)
           connection.socket.salaId = salaId
         } else if (data.type === 'LOCALIZATION') {
-          console.log('entrou no if localization')
+          console.log('inicio if localization')
           const { salaId } = data
           if (!salas[salaId]) {
             salas[salaId] = new Set()
           }
+          console.log('final if localization')
         }
+        console.log('final if data type')
       } else {
         console.log('entrou no else do data.type')
         const sala = connection.socket.salaId
@@ -35,9 +37,7 @@ export async function localizacaoRoutes(app: FastifyInstance) {
           })
         }
       }
-
-      // const data = message.toString()
-      connection.socket.send(data)
+      console.log('final da função') // const data = message.toString()
     })
   })
 }
