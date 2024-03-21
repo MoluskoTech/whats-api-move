2e65864c798d683f277e6eb36da0106cd68cee97
Carlos Cesar Baratella Junior <molusko.rp@hotmail.com>
2024-03-21 09:04:54 -0300

    asd

--- src/routes/localizacao.route.ts
+++ src/routes/localizacao.route.ts
@@ -3,7 +3,8 @@ import { FastifyInstance } from 'fastify'
 export async function localizacaoRoutes(app: FastifyInstance) {
   app.get('/', { websocket: true }, async (connection, req) => {
     connection.socket.on('message', (message) => {
-      console.log(message.toString())
+      const data = message.toString()
+      connection.socket.send(data)
     })
 
     connection.socket.on('open', () => {
